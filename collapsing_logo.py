f956080f1ee926b317b882a608c03f943b24b408
Jan Vos <jan.vos@hva.nl>
2025-07-11 15:07:25 +0200

    start of phase 2

--- collapsing_logo.py
+++ collapsing_logo.py
@@ -2,10 +2,11 @@ import pyautogui
 
 pyautogui.FAILSAFE = True
 
-duration = 0.7
-switch = 2 
-
-column = 0
+duration = 0.8 # duration of action
+switch = 2 # bottom and top
+coljumpX = 120 # represents the amount of pixels to shift sideways
+column = 0 # current column, maximum of 14 (15 total)
+coljumpCorrection = 30 # pixels to shit downwards to successfully paste the top portion
 
 startingX = 112
 StartingY = 760
@@ -16,54 +17,62 @@ phaseOneCopyY1 = 655
 PhaseOnePasteX1 = 66
 PhaseOnePasteY1 = 700
 
-def clear_selected_pixels(clearX1, clearY1, clearX2, clearY2, phase):
+def select_copy_and_clear_pixels(x1, y1, x2, y2):
+    pyautogui.moveTo(x1, y1, duration=duration)
+
     pyautogui.keyDown('ctrl')
+    pyautogui.dragTo(x2, y2, duration=duration)
+    pyautogui.hotkey('c', interval=duration)
+
     pyautogui.keyDown('shift')
-
-    pyautogui.moveTo(clearX1, clearY1, duration=duration)
-    pyautogui.dragTo(clearX2, clearY2, duration=duration)
-
+    pyautogui.moveTo(x1, y1, duration=duration)
+    pyautogui.dragTo(x2, y2, duration=duration)
+    
     pyautogui.keyUp('ctrl')
     pyautogui.keyUp('shift')
 
-def select_and_copy_pixels(copyX1, copyY1, copyX2, copyY2):
-    pyautogui.moveTo(copyX1, copyY1, duration=duration)
-
-    pyautogui.keyDown('ctrl')
-    pyautogui.dragTo(copyX2, copyY2, duration=duration)
-    pyautogui.hotkey('c', interval=duration)
-
-def paste_copied_pixels(pasteX1, pasteY1):
+def paste_copied_pixels(x1, y1):
     pyautogui.keyDown('ctrl')
     pyautogui.hotkey('v', interval=duration)
     pyautogui.keyUp('ctrl')
 
-    pyautogui.moveTo(pasteX1, pasteY1, duration=duration)
+    pyautogui.moveTo(x1, y1, duration=duration)
     pyautogui.leftClick()
 
 def right_or_left(type, phase):
     for i in range(phase):
-        pyautogui.hotkey(f'{type}', interval=duration)
+        pyautogui.keyDown(f'{type}')
+        pyautogui.keyUp(f'{type}')
 
-def phaseOne():
+def phase_one():
     phase = 1
     coljumpX = 120
     coljumpY = -360
+    coljumpCorrection = 30
 
     for i in range(switch):
-        select_and_copy_pixels(startingX + (column * coljumpX), 
+        select_copy_and_clear_pixels(startingX + (column * coljumpX), 
                                StartingY + (i * coljumpY), 
                                phaseOneCopyX1 + (column * coljumpX), 
                                phaseOneCopyY1 + (i * coljumpY))
-        
-        clear_selected_pixels(startingX + (column * coljumpX), 
-                              StartingY + (i * coljumpY), 
-                              phaseOneCopyX1 + (column * coljumpX), 
-                              phaseOneCopyY1 + (i * coljumpY), phase)
-        
+
         right_or_left('right', phase)
-        paste_copied_pixels(PhaseOnePasteX1 + (column * coljumpX), PhaseOnePasteY1 + (i * (coljumpY+30)))
+        paste_copied_pixels(PhaseOnePasteX1 + (column * coljumpX), PhaseOnePasteY1 + (i * (coljumpY+coljumpCorrection)))
         right_or_left('left', phase)
 
-phaseOne()
+def phase_two():
+    phase = 2
+
+    for i in range(switch):
+        coljumpY = -170
+        select_copy_and_clear_pixels(111 + (column * coljumpX), 
+                               624 + (i * coljumpY), 
+                               7 + (column * coljumpX), 
+                               595 + (i * coljumpY))
+
+        right_or_left('right', phase)
+        paste_copied_pixels(67 + (column * coljumpX), 594 + (i * (coljumpY+coljumpCorrection)))
+        right_or_left('left', phase)
+
+phase_two()
 #print(pyautogui.position())
